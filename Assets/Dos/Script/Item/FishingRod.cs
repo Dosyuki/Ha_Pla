302c6f62869c51981970bfc42c2719130f68893f
xFallerz <arromdeedos@gmail.com>
2025-08-02 20:34:13 +0700

    FishingRod Line(string)

--- Assets/Dos/Script/Item/FishingRod.cs
+++ Assets/Dos/Script/Item/FishingRod.cs
@@ -11,20 +11,28 @@ public class FishingRod : BaseItem
     [SerializeField] private MouseLook mouseLook;
 
     [SerializeField] private Transform baitTransform;
-    [SerializeField] Transform rodTip;
+    [SerializeField] private Transform rodTip;
+
     private Rigidbody bait;
-    private TrailRenderer trail;
+    private LineRenderer lineRenderer;
     private bool isRecalling = false;
+
     private void Start()
     {
         Prefab = this.gameObject;
         bait = Prefab.GetComponentInChildren<Rigidbody>();
-        trail = bait.GetComponent<TrailRenderer>();
-        trail.enabled = false;
+        baitTransform = bait.transform;
+
         playerController = FindObjectOfType<FirstPersonController>();
         mouseLook = playerController.GetMouseLook();
-        
-        baitTransform = bait.transform;
+
+        lineRenderer = bait.GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 20;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        lineRenderer.receiveShadows = false;
     }
 
     private void Update()
@@ -43,15 +51,40 @@ public class FishingRod : BaseItem
         {
             RecallBaitLerp();
         }
+
         FishingHook();
+        if (isThrown || isRecalling)
+        {
+            UpdateLine();
+        }
     }
 
     private void FishingHook()
     {
         if (Physics.OverlapSphere(baitTransform.position, 0.6f, FishingLayer).Length != 0)
         {
-            GetComponent<Rigidbody>().isKinematic = true;
+            bait.isKinematic = true;
             Debug.Log("Start Playing Minigame");
+        }
+    }
+
+    private void UpdateLine()
+    {
+        int segmentCount = lineRenderer.positionCount;
+        Vector3 start = rodTip.position;
+        Vector3 end = baitTransform.position;
+
+        for (int i = 0; i < segmentCount; i++)
+        {
+            float t = (float)i / (segmentCount - 1);
+            Vector3 point = Vector3.Lerp(start, end, t);
+
+            float distance = Vector3.Distance(start, end);
+            float curveAmount = Mathf.Clamp(distance * 0.1f, 0f, 2f);
+            float sag = Mathf.Sin(t * Mathf.PI) * curveAmount;
+            point.y -= sag;
+
+            lineRenderer.SetPosition(i, point);
         }
     }
 
@@ -59,19 +92,19 @@ public class FishingRod : BaseItem
     {
         isThrown = true;
         isRecalling = false;
-        
+
         bait.isKinematic = false;
-        trail.enabled = true;
+        lineRenderer.enabled = true;
+
         Vector3 throwDirection = (bait.transform.forward + Vector3.up).normalized;
         bait.AddForce(throwDirection * ThrowPower, ForceMode.Impulse);
-        playerController.enabled = false;
-        mouseLook.SetCursorLock(false);
+        bait.transform.parent = null;
     }
 
     private void StartRecall()
     {
         isRecalling = true;
-        bait.isKinematic = true; // stop physics so we can move it manually
+        bait.isKinematic = true;
     }
 
     private void RecallBaitLerp()
@@ -79,17 +112,16 @@ public class FishingRod : BaseItem
         float step = RecallSpeed * Time.deltaTime;
         baitTransform.position = Vector3.MoveTowards(baitTransform.position, rodTip.position, step);
 
-        // Stop recalling when it's close enough
         if (Vector3.Distance(baitTransform.position, rodTip.position) < 0.1f)
         {
             isRecalling = false;
             isThrown = false;
-            trail.enabled = false;
-            
-            playerController.enabled = true;
-            mouseLook.SetCursorLock(true);
+            lineRenderer.enabled = false;
+
+            bait.transform.parent = rodTip.transform;
         }
     }
+
     public bool getIsThrown() => isThrown;
     public LayerMask getFishingLayer() => FishingLayer;
 }
